1debefefc2f1d7debef59af06fed9dca4a6cd257
hyder <yanghuan0613@gmail.com>
2019-05-31 12:39:22 +0800

    redux初识

---
+++ src/TodoList.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import {Button, Card, Input, List} from "antd";
+
+import 'antd/dist/antd.css'
+
+import store from './store'
+
+export default class TodoList extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = store.getState();
+  }
+
+  render() {
+    return (
+      <Card title={"TODOLIST"} style={{marginTop: 10, marginLeft: 10}}>
+        <Input value={this.state.inputValue} placeholder={"todoInfo"} style={{width: '300px'}}/>
+        <Button type={"primary"}>提交</Button>
+        <List
+          style={{marginTop: 10, width: 300}}
+          bordered
+          dataSource = {this.state.list}
+          renderItem={item => (
+            <List.Item>{item}</List.Item>
+          )}
+        >
+        </List>
+      </Card>
+    );
+  }
+}
